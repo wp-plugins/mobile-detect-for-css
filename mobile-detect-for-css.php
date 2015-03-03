c4f0e593e49ace784dc106780033bae6ab41aa3d
svincoll4 <svincoll4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-03 04:15:29 +0000

    add new version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/mobile-detect-for-css/tags/1.1@1103805 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mobile-detect-for-css.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin Name: Mobile Detect For CSS
+Description: This plugin add the device css class to BODY tag
+Version: 1.1
+Author: Phuc Pham
+License: GPL
+Copyright: Phuc Pham
+*/
+
+if(!class_exists('Mobile_Detect')){
+    require_once 'Mobile_Detect.php';
+}
+
+class Mobile_Detect_For_Css{
+    var $mobile_detect;
+    public function __construct(){
+
+        $this->mobile_detect = new Mobile_Detect();
+
+        add_filter('body_class', array($this, 'body_class'));
+    }
+
+    function body_class($classes) {
+
+        if($this->mobile_detect->isMobile()){
+            $classes[] = 'mobile';
+        }
+
+        if($this->mobile_detect->isTablet()){
+            $classes[] = 'tablet';
+        }
+
+        if( $this->mobile_detect->isiOS() ){
+            $classes[] = 'ios';
+        }
+
+        if( $this->mobile_detect->isAndroidOS() ){
+            $classes[] = 'android';
+        }
+
+        if( $this->mobile_detect->isMobile() && $this->mobile_detect->isTablet() ){
+            $classes[] = 'handheld';
+        }
+
+        if(!$this->mobile_detect->isMobile() && !$this->mobile_detect->isTablet()){
+            $classes[] = 'desktop';
+        }
+
+        return $classes;
+    }
+}
+
+new Mobile_Detect_For_Css;
+
